9e43045b83fc90969e8ed040785aca47028cafed
legian-andrei <andreilegian138@yahoo.com>
2024-03-05 21:38:56 +0200

    ex4

--- lab1/src/lab1/task1/Main.java
+++ lab1/src/lab1/task1/Main.java
@@ -2,14 +2,18 @@ package lab1.task1;
 
 import lab1.task2.Course;
 import lab1.task2.StudentAllocator;
+import lab1.task4.DummyCalculator;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Course test = new Course("mate", 5, StudentAllocator.createStudents());
-        System.out.println("chooseStudentRandomly: " + test.chooseStudentRandomly());
-        System.out.println("isStudentPassing: " + test.isStudentPassing(2));
-        System.out.println("showAllPassingStudents: " + Arrays.toString(test.showAllPassingStudents()));
+//        Course test = new Course("mate", 5, StudentAllocator.createStudents());
+//        System.out.println("chooseStudentRandomly: " + test.chooseStudentRandomly());
+//        System.out.println("isStudentPassing: " + test.isStudentPassing(2));
+//        System.out.println("showAllPassingStudents: " + Arrays.toString(test.showAllPassingStudents()));
+        DummyCalculator test = new DummyCalculator();
+        test.calculate();
+
     }
 }
--- lab1/src/lab1/task4/DummyCalculator.java
+++ lab1/src/lab1/task4/DummyCalculator.java
@@ -1,22 +1,61 @@
 package lab1.task4;
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
 public class DummyCalculator {
-    private int calculateI (int p1, String op, int p2){
-
+    private boolean isInteger(double x){
+        return (int) x == x;
+    }
+    private void calculateD (double p1, String op, double p2){
+        switch (op) {
+            case "+":
+                System.out.println((isInteger(p1 + p2) ? (int) (p1 + p2) : p1 + p2));
+                break;
+            case "-":
+                System.out.println(isInteger(p1 - p2) ? (int) (p1 - p2) : p1 - p2);
+                break;
+            case "*":
+                System.out.println(isInteger(p1 * p2) ? (int) (p1 * p2) : p1 * p2);
+                break;
+            case "/":
+                if (p2 != 0) {
+                    System.out.println(isInteger(p1 / p2) ? (int) (p1 / p2) : p1 / p2);
+                } else {
+                    System.out.println(Double.NaN);
+                }
+                break;
+            default:
+                System.out.println(Double.NaN);
+                break;
+        }
+    }
+    private void calculateB (boolean p1, String op, boolean p2){
+        switch (op) {
+            case "&&":
+                System.out.println(p1 && p2);
+                break;
+            case "||":
+                System.out.println(p1 || p2);
+                break;
+            default:
+                System.out.println(false);
+                break;
+        }
     }
     public void calculate(){
         Scanner sc = new Scanner(System.in);
-        String term1 = sc.next();
-        String op = sc.next();
-        String term2 = sc.next();
-        try {
-            Integer.parseInt(term1);
-            System.out.println(calculateI(Integer.parseInt(term1), op, Integer.parseInt(term2)));
-        } catch (NumberFormatException) {
-
+        while (true) {
+            String term1 = sc.next();
+            if (term1.contains("quit")) {
+                sc.close();
+                return;
+            }
+            String op = sc.next();
+            String term2 = sc.next();
+            try {
+                calculateD(Double.parseDouble(term1), op, Double.parseDouble(term2));
+            } catch (NumberFormatException e) {
+                calculateB(Boolean.parseBoolean(term1), op, Boolean.parseBoolean(term2));
+            }
         }
     }
 }
